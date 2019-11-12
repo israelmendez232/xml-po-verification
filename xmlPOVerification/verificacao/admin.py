6406caad2ed9f84349bf0e0916f998b95520261e
Israel Mendes <israel.mendez232@gmail.com>
2019-11-12 07:32:39 -0300

    Elementos visuais novos do django.

--- xmlPOVerification/verificacao/admin.py
+++ xmlPOVerification/verificacao/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Verificação
+from .models import Verificação 
 
-admin.site.register(Verificação)
+@admin.register(Verificação)
+class Verificação(admin.ModelAdmin):
+
+    def response_change(self, request, obj):
+        if "_make-unique" in request.POST:
+            self.message_user(request, "Teste de Verificação XML")
+            return HttpResponseRedirect(".")
+        return super().response_change(request, obj)
--- xmlPOVerification/verificacao/models.py
+++ xmlPOVerification/verificacao/models.py
@@ -6,6 +6,7 @@ class Verificação(models.Model):
     datetime =  datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     xml = models.FileField('XML:', upload_to='XMLs', blank=True)    
-    po = models.FileField('Ordem de Compra:', upload_to='POs', blank=True)   
+    po = models.FileField('Ordem de Compra:', upload_to='POs', blank=True)
+      
     def __str__(self):        
         return self.datetime
